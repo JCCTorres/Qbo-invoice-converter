e3b9431eb2e16a324f20b7e6632aabb692149d96
Joao Torres <118075830+JCCTorres@users.noreply.github.com>
2025-04-17 03:47:15 +0100

    Add run_app.bat

---
+++ run_app.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Starting QBO Invoice Converter...
+echo.
+echo This window will show the application logs. Do not close it!
+echo.
+echo Access the application by opening your web browser and going to:
+echo http://localhost:5000
+echo.
+echo Press Ctrl+C followed by 'Y' to stop the server when you're done.
+echo.
+echo Starting...
+echo ===============================================================
+python app.py
+pause
